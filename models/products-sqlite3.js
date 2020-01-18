411e8d9bde4812bee57467dd9a75882e889f598d
Szymon Gabrek <szymon.gabrek@gmail.com>
2020-01-18 12:44:08 +0100

    add simple sqlite3 model

---
+++ models/products-sqlite3.js
@@ -0,0 +1,21 @@
+const Product = require('./Product');
+const sqlite3 = require('sqlite3');
+const DBG = require('debug');
+const debug = DBG('products:products-sqlite3');
+const error = DBG('products:error-sqlite3');
+
+let db; // DB Connection here
+
+async function connectDB() {
+    if (db) return db;
+    const dbfile = process.env.SQLITE_FILE || 'db.sqlite3';
+    await new Promise((resolve, reject) => {
+        db = new sqlite3.Database(dbfile,
+            sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+            err => {
+                if (err) return reject(err);
+                resolve(db);
+            });
+    });
+    return db;
+}
