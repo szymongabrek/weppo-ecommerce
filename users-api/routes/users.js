d06df1dd0fa3c700c40a7d18128e1352030326aa
Naylepsh <naylepsh@gmail.com>
2020-01-25 20:14:16 +0100

    add UPDATE and DESTROY routes

--- users-api/routes/users.js
+++ users-api/routes/users.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+function sendError(res, err, backupCode) {
+  res.status(err.status || backupCode).json({
+    message: err.message,
+    error: err
+  });
+}
+
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
   const users = await User.findAll();
@@ -12,19 +19,42 @@ router.post('/', async (req, res, next) => {
   try {
     const user = await User.create(req.body.user);
     res.json(user);
-  } catch(e) {
+  } catch(err) {
     // find some better error code
-    res.status(500).send('Internal Server Error');
+    sendError(res, err, 500);
   }
 });
 
-router.get('/{pk}', async (req, res, next) => {
+router.get('/:pk', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.pk);
     res.json(user);
   } catch(e) {
-    // implement better error handling?
-    res.status(404).send('not found')
+    sendError(res, err, 404);
+  }
+});
+
+router.put('/:pk', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.pk);
+    for (const key in req.body.user) {
+      user[key] = req.body.user[key];
+    }
+    const updatedUser = await user.save();
+    res.json(updatedUser);
+  } catch(e) {
+    // find better error code
+    sendError(res, err, 500);
+  }
+});
+
+router.delete('/:pk', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.pk);
+    await user.destroy();
+    res.status(204).send('No Content');
+  } catch(err) {
+    sendError(res, err, 500);
   }
 });
 
