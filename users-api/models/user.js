9f790ae9e3f1d3afb6f2e817cae237e90f582b90
Naylepsh <naylepsh@gmail.com>
2020-01-26 12:18:19 +0100

    add password hashing

--- users-api/models/user.js
+++ users-api/models/user.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../database/config');
 const Model = Sequelize.Model;
+const bcrypt = require('bcrypt-nodejs');
 
 class User extends Model {}
 User.init({
@@ -8,5 +9,37 @@ User.init({
     password: Sequelize.TEXT,
     role: Sequelize.TEXT
 }, { sequelize, modelName: 'user' });
+
+// User.beforeCreate( (user, options) => {
+//     return encryptPassword(user.password)
+//     .then (success => {
+//         user.password = success;
+//     })
+// });
+
+User.beforeSave( (user, options) => {
+    return encryptPassword(user.password)
+    .then(success => {
+        user.password = success
+    })
+    .catch(err => {
+        console.error(err);
+    });
+});
+
+function encryptPassword(password) {
+    const saltRounds = 10;
+    return new Promise( (resolve, reject) => {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) return reject(err);
+            bcrypt.hash(password, salt, null, (err, hash) => {
+                if (err) return reject(err);
+                return resolve(hash);
+            })
+        })
+    })
+}
+
+User.sync();
 
 module.exports = User;
