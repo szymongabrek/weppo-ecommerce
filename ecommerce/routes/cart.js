bb28156cb91ed9e5dba702710f95c3579cec2cfd
Naylepsh <naylepsh@gmail.com>
2020-01-29 18:52:49 +0100

    let user remove items from cart

--- ecommerce/routes/cart.js
+++ ecommerce/routes/cart.js
@@ -6,14 +6,19 @@ const products = require('../models/products-sequelize');
 let cart = new Cart([]);
 
 router.get('/', (req, res) => {
-    let lines = cart.cartLines;
-    res.send(lines);
+    let cartlines = cart.cartLines;
+    res.render('cart/view', { title: 'Cart', cartlines });
 });
 
 router.get('/add/:id', async (req, res) => {
     const product = await products.read(req.params.id);
     cart.addProduct(product, 1);
-    res.redirect('/cart');
+    res.redirect('/products');
 });
+
+router.get('/remove/:id', async (req, res) => {
+    cart.removeProduct(req.params.id);
+    res.redirect('/cart');
+})
 
 module.exports = router;
