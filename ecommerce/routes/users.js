fe1f48aea6ce184e26474df8632d6e33f7dab5a6
Szymon Gabrek <szymon.gabrek@gmail.com>
2020-01-31 10:38:33 +0100

    add users route

--- ecommerce/routes/users.js
+++ ecommerce/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+import path from 'path';
+)const util = require('util');
+const express = require('express'); 
+const passport = require('passport'); 
+const passportLocal = require('passport-local');
+const LocalStrategy = passportLocal.Strategy; 
+const usersModel= require('../models/users-superagent');
+const { sessionCookieName } = require('../app');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const router = express.Router();
+
+const debug = require('debug')('ecommerce:router-users'); 
+const error = require('debug')('ecommerce:error-users'); 
 
 module.exports = router;
