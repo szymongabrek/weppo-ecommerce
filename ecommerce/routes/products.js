e5206cac3b1e295d3a5ee2add71a8a3ee1603e80
Naylepsh <naylepsh@gmail.com>
2020-02-02 23:36:42 +0100

    start orders

--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const products = require('../models/products-sequelize');
-const faker = require('faker');
 const { ensureAuthenticated, ensurePermissions } = require('./users'); 
 
 /* GET home page. */
